05c76954ed49bc978906a1bff6cb588fd16c3b33
Kai Zhu <22319143@zju.edu.cn>
2025-10-28 10:12:42 +0800

    Add files via upload

---
+++ chignolin/run_biased_multi_gnn/mkdir.sh
@@ -0,0 +1,8 @@
+    mkdir -p 0; cp md.tpr 0/md.tpr; cp plumed.dat 0/plumed.dat 
+    mkdir -p 1; cp md.tpr 1/md.tpr; cp plumed.dat 1/plumed.dat
+    mkdir -p 2; cp md.tpr 2/md.tpr; cp plumed.dat 2/plumed.dat
+    mkdir -p 3; cp md.tpr 3/md.tpr; cp plumed.dat 3/plumed.dat
+    mkdir -p 4; cp md.tpr 4/md.tpr; cp plumed.dat 4/plumed.dat
+    mkdir -p 5; cp md.tpr 5/md.tpr; cp plumed.dat 5/plumed.dat
+    mkdir -p 6; cp md.tpr 6/md.tpr; cp plumed.dat 6/plumed.dat
+    mkdir -p 7; cp md.tpr 7/md.tpr; cp plumed.dat 7/plumed.dat
